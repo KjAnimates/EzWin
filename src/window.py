0fd92e721c1f45f82f7760a94edf480c80003f80
KJ <kjhouser43@gmail.com>
2024-01-05 22:00:34 -0500

    Create window property

--- src/window.py
+++ src/window.py
@@ -3,6 +3,7 @@ import tkinter as tk
 class Window:
     ''' title of the window. '''
     title: str = ""
+    window: tk.Tk | None = None
 
     def __init__(self, title: str):
         if title != None and title is str:
