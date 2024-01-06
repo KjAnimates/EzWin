c756e9d03a8155fcc10c12ce2775f88379747608
KJ <kjhouser43@gmail.com>
2024-01-05 22:13:35 -0500

    Checking the types of width and height

--- src/window.py
+++ src/window.py
@@ -8,5 +8,9 @@ class Window:
     width = 0
     height = 0
 
-    def __init__(self):
-        pass
+    def __init__(self, width: int, height: int):
+        if type(width) is int:
+            self.width = width
+        if type(height) is int:
+            self.height = height
+
