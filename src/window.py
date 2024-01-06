81e140cbe7ef7a5e3ad5e16fb518e042e90c3eba
KJ <kjhouser43@gmail.com>
2024-01-05 21:46:36 -0500

    Create title property | create __init__ function

--- src/window.py
+++ src/window.py
@@ -1,2 +1,7 @@
 class Window:
-    pass
+    ''' title of the window. '''
+    title: str = ""
+
+    def __init__(self, title: str):
+        if title != None and title is str:
+            self.title = title
