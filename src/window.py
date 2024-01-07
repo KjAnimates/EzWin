96889e49a1b95368e0a6aae386061a7889e3f836
KJ <kjhouser43@gmail.com>
2024-01-07 12:37:14 -0500

    Create comments

--- src/window.py
+++ src/window.py
@@ -27,11 +27,15 @@ class Window:
         `ValueError` -> Happens when either `width` or `height` is `None` or not an integer.
     '''
     def set_size(self, width: int, height: int):
+        # If the width isn't none, and it is an integer, set this instance's width to it.
+        # Otherwise, raise a ValueError.
         if width != None and type(width) is int:
             self.width = width
         else:
             raise ValueError("Width must be an integer and must not be `None`.")
         
+        # If the height isn't none, and it is an integer, set this instance's height to it.
+        # Otherwise, raise a ValueError.
         if height != None and type(height) is int:
             self.height = height
         else:
@@ -39,6 +43,9 @@ class Window:
     
     ''' Displays the window. '''
     def display(self):
+        # If this instance's window exists,
+        #  * Set the window's title to this title
+        # Otherwise, warn the user that the window did not open.
         if self.window != None:
             self.window.title(self.title)
             self.window.mainloop()
@@ -52,6 +59,10 @@ class Window:
         `ValueError` -> Caused by `new_title` not existing or not being of type `str`.
     '''
     def set_title(self, new_title: str = "Untitled Window"):
+        # If `new_title` exists,
+        # 1. Check if it is a string
+        # 2. If it is a string, set this title to `new_title`.
+        # 3. Otherwise, raise a ValueError.
         if new_title != None:
             if type(new_title) is str:
                 self.title = new_title
@@ -62,7 +73,7 @@ class Window:
     @param new_geometry: The new geometry of the window (`width`x`height`)
     '''
     def set_geometry(self, new_geometry: str):
+        # If `new_geometry` exists, and it's a string, set the window's geometry to `new_geometry`.
         if new_geometry != None:
             if type(new_geometry) is str:
                 self.window.geometry(new_geometry)
-
